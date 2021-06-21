c4ab7ab786bdc41bea81155c6edafb8386ac5e2d
Marc Muñoz Fernandez <84795384+mmunoz28@users.noreply.github.com>
2021-06-21 15:53:20 +0200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,9 @@
+while True:
+
+    num1 = input("Ingrese Un Numero:")
+    num2 = input("Ingrese Otro Numero:")
+
+    res = float(num1) + float(num2)
+
+    print("La Suma Es:", res)
+    print("!Gracias!")
